86db0363da95d3a2ba81de0d8663cbe7c5bd3e5e
泥沼 <116502469+NiZhao216@users.noreply.github.com>
2026-03-30 22:07:19 +0800

    1

--- WebApplication4/Controllers/Home/HomeController.cs
+++ WebApplication4/Controllers/Home/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApplication4.Controllers.Home
 {
     [Authorize]
+    [Authorize(Roles ="普通用户")]
     public class HomeController : Controller
     {
         public IActionResult Index()
